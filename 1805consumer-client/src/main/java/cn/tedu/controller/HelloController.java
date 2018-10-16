0f026a293d5a0aa1a5bab4b8be2ba1470616e498
matao <823694776@qq.com>
2018-10-16 11:42:58 +0800

    test

---
+++ 1805consumer-client/src/main/java/cn/tedu/controller/HelloController.java
@@ -0,0 +1,20 @@
+package cn.tedu.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class HelloController {
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	@RequestMapping("/hello/{name}")
+	public String hello(@PathVariable String name){
+		String url="http://localhost:7900/hello/"+name;
+		url="http://provider-user:7900/hello/"+name;
+		return restTemplate.getForObject(url, String.class);
+	}
+}
